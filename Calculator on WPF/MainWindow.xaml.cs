3e8f22ff63facf3900f794f3037676378ecd47ec
PHPpriest <44893659+PHPpriest@users.noreply.github.com>
2020-10-26 09:52:08 +0500

    Добавил функции калькулятора. Рабочая версия калькулятора завершена.

--- Calculator on WPF/MainWindow.xaml.cs
+++ Calculator on WPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,50 @@ namespace Calculator_on_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool isSolved = false;
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+            foreach (UIElement element in MainContainer.Children)
+            {
+                if (element is Button)
+                {
+                    ((Button) element).Click += ButtonClick;
+                }
+            }
         }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (textLabel_1.Text.Length == 35) { textLabel_1.FontSize = 13; }
+
+            if (isSolved == true)
+            {
+                textLabel_1.Text = "";
+                isSolved = false;
+            }
+
+            // Мы берем объект, который идет на основе класса RoutedEventArgs, преобразовываем его к классу
+            // button. Далее берется OriginalSource и его Content. И всё это присваивается к переменной enteredButtons.
+            string enteredButton = (string)((Button)e.OriginalSource).Content;
+
+            if (enteredButton == "Clear") 
+                textLabel_1.Text = ""; 
+            else if (enteredButton == "=")
+            {
+                // Метод Compute отвечает за преобразование текста в математическое действие
+                string value = new DataTable().Compute(textLabel_1.Text, null).ToString();
+                textLabel_1.Text += ("\n = " + value);
+
+                isSolved = true;
+            }
+            else  
+                textLabel_1.Text += enteredButton; 
+        }
+
+
     }
 }
